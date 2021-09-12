bab6dfe75648d1338eff6f9c1c6e335d00b4d113
davidberiro <deebenchimol@gmail.com>
2021-09-12 15:47:38 -0400

    cleanup

--- contracts/interfaces/IUnicFarm.sol
+++ contracts/interfaces/IUnicFarm.sol
@@ -4,14 +4,6 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IUnicFarm {
-    //struct PoolInfo {
-        //IERC20 lpToken; // Address of LP token contract.
-        //uint256 allocPoint; // How many allocation points assigned to this pool. UNICs to distribute per block.
-        //uint256 lastRewardBlock; // Last block number that UNICs distribution occurs.
-        //uint256 accUnicPerShare; // Accumulated UNICs per share, times 1e12. See below.
-        //address uToken;
-    //}
-
     function pendingUnic(uint256 _pid, address _user) external view returns (uint256);
 
     function deposit(uint256 _pid, uint256 _amount) external;
